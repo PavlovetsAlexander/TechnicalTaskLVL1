af22c6da497cdaac857ce03cbebd0ab78a37ff99
Pavlovets Alexander <>
2024-12-11 17:03:23 +0100

    ADD ApiRoutable

---
+++ TechnicalTask-lvl1/NetworkLayer/ApiRoutable.swift
@@ -0,0 +1,23 @@
+//
+//  ApiRoutable.swift
+//  TechnicalTask-lvl1
+//
+//  Created by Alexander Pavlovets on 11.12.24.
+//
+
+import Foundation
+
+enum HttpMethod: String {
+    case post = "POST"
+    case get = "GET"
+    case delete = "DELETE"
+}
+
+protocol ApiRoutable {
+    var url: URL { get }
+    var path: String { get }
+    var method: HttpMethod { get }
+    var headers: [String: String]? { get }
+    var query: [String: Any]? { get }
+    var body: [String: Any]? { get }
+}
