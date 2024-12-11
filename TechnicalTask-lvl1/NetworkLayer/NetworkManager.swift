3223f779ca3fc75f7a57e7e1b0b9f590397221e0
Pavlovets Alexander <>
2024-12-11 18:35:20 +0100

    UPDATE NetworkManager

--- TechnicalTask-lvl1/NetworkLayer/NetworkManager.swift
+++ TechnicalTask-lvl1/NetworkLayer/NetworkManager.swift
@@ -13,13 +13,42 @@ protocol NetworkManager {
 }
 
 struct NetworkManagerImpl: NetworkManager {
+    // MARK: - Properties
+    private let session: URLSession
+    private let urlBuilder: URLBuilder
 
-    func request<T>(route: APIRoutable,
-                    completion: @escaping (Result<T, any Error>) -> Void) where T : Decodable {
-        
+    // MARK: - Initialization
+    init(session: URLSession = .shared, urlBuilder: URLBuilder) {
+        self.session = session
+        self.urlBuilder = urlBuilder
     }
 
-    private func decodeData() {
+    //MARK: - Methods
+    func request<T>(route: APIRoutable,
+                    completion: @escaping (Result<T, any Error>) -> Void) where T : Decodable {
+        do {
+            let request = try urlBuilder.build(with: route)
+            let task = session.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    completion(.failure(APIError.serverError(message: error.localizedDescription)))
+                    return
+                }
 
+                guard let data else {
+                    completion(.failure(APIError.serverError(message: "No data received")))
+                    return
+                }
+
+                do {
+                    let decodedResponse = try JSONDecoder().decode(T.self, from: data)
+                    completion(.success(decodedResponse))
+                } catch {
+                    completion(.failure(APIError.decodedError))
+                }
+            }
+            task.resume()
+        } catch {
+            completion(.failure(APIError.invalidResponse))
+        }
     }
 }
