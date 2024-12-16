14490bb588ec54fb02ea932d6a7d33ac204cd262
Pavlovets Alexander <alexander.pavlovets@innowise.com>
2024-12-16 09:26:00 +0100

    UPDATE APIError

--- TechnicalTask-lvl1/NetworkLayer/APIError.swift
+++ TechnicalTask-lvl1/NetworkLayer/APIError.swift
@@ -11,8 +11,10 @@ enum APIError: Error {
     case invalidURL
     case invalidResponse
     case invalidData
+    case invalidRequest(message: String)
     case decodedError(message: String)
     case serverError(message: String)
+    case connectionError(message: String)
 
     var errorDescription: String? {
         switch self {
@@ -23,8 +25,12 @@ enum APIError: Error {
         case .invalidURL:
             return "Invalid URL"
         case .decodedError(message: let message):
-            return "Decoding error"
+            return message
         case .serverError(message: let message):
+            return message
+        case .connectionError(message: let message):
+            return message
+        case .invalidRequest(message: let message):
             return message
         }
     }
