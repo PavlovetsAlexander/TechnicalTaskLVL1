b223672012a9586c00a17caae153434d64ba6c79
Pavlovets Alexander <alexander.pavlovets@innowise.com>
2024-12-16 09:31:35 +0100

    ADD UserTableViewCell

---
+++ TechnicalTask-lvl1/UserModule/Views/UserTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  UsersTableViewCell.swift
+//  TechnicalTask-lvl1
+//
+//  Created by Alexander Pavlovets on 16.12.24.
+//
+
+import UIKit
+
+final class UserTableViewCell: UITableViewCell {
+    // MARK: - GUI Properties
+
+    // MARK: - Initialization
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setupCell()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Methods
+    private func setupCell() {
+
+    }
+}
